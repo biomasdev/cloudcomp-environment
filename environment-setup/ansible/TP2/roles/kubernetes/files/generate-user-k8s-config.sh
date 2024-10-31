49df1bfafbd6d0f7d7a3a0985442d742bf9b6aba
Matheus Alves <matheus@biomas.dev>
2024-10-31 19:53:15 -0300

    improvements to k8s configuration - better storag

--- environment-setup/ansible/TP2/roles/kubernetes/files/generate-user-k8s-config.sh
+++ environment-setup/ansible/TP2/roles/kubernetes/files/generate-user-k8s-config.sh
@@ -2,42 +2,42 @@
 set -eu
 
 STUDENT=$1
+CURRENT_MACHINE_IP=$2
+CLUSTER_NAME=$3
 echo "Setting up user: $STUDENT"
 
-# We added a token generation entry to the k8s-users.yml to address the
-# change in K8s 1.24, which no longer adds secrets to serviceaccounts by
-# default:
-# https://itnext.io/big-change-in-k8s-1-24-about-serviceaccounts-and-their-secrets-4b909a4af4e0
+set -o errexit
 
-token=$(kubectl -n "${STUDENT}" describe secret "${STUDENT}" | grep token: | awk '{print $2;}')
-certs=$(kubectl -n "${STUDENT}" get secret "${STUDENT}" -o "jsonpath={.data.ca\.crt}")
+token=$(kubectl -n "${STUDENT}" get secret/"${STUDENT}" -o jsonpath="{.data.token}" | base64 --decode)
+certs=$(kubectl -n "${STUDENT}" get secret/"${STUDENT}" -o jsonpath="{.data.ca\.crt}")
 
 mkdir -p "/home/$STUDENT/.kube/"
 
 echo """
 apiVersion: v1
+kind: Config
+
 clusters:
 - cluster:
     certificate-authority-data: $certs
-    server: https://{{ current_machine_ipaddress }}:6443
-  name: {{ cluster_name }}
+    server: https://$CURRENT_MACHINE_IP:6443
+  name: $CLUSTER_NAME
 
 contexts:
 - context:
-    cluster: {{ cluster_name }}
-    namespace: $STUDENT
+    cluster: $CLUSTER_NAME
     user: $STUDENT
-  name: $STUDENT
+    namespace: $STUDENT
+  name: $STUDENT@$CLUSTER_NAME
 
-current-context: $STUDENT
-kind: Config
-preferences: {}
+current-context: $STUDENT@$CLUSTER_NAME
 
 users:
 - name: $STUDENT
   user:
      token: $token
-     client-key-data: $certs
 """ > "/home/$STUDENT/.kube/config"
 
+cp "/home/$STUDENT/.kube/config" "/home/$STUDENT/kube_config_cluster.yaml"
 sudo chown "$STUDENT:$STUDENT" -R "/home/$STUDENT/.kube"
+sudo chown "$STUDENT:$STUDENT" -R "/home/$STUDENT/kube_config_cluster.yaml"
